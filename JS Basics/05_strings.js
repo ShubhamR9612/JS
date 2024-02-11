82d923e058033fd081fa6b2aba4f8fe4b8485f7d
shubham <shubham@algdev.in>
2024-02-11 22:42:25 +0530

    strings

---
+++ JS Basics/05_strings.js
@@ -0,0 +1,34 @@
+const myName = "Shubham";
+const age = 24;
+
+//console.log("Hi, My name is "+name+" and I am "+ age+" years old.");
+
+console.log(`Hi, My name is ${myName} and I am ${age} years old.`);
+
+
+const gameName = new String('Football');
+
+console.log(gameName);
+console.log(gameName[2]);
+
+console.log(gameName.toUpperCase());
+
+console.log(gameName.charAt(2));
+console.log(gameName.indexOf('l'));
+
+const newString = gameName.substring(0,3);
+console.log(newString);
+
+
+const newString2 = gameName.slice(-4,3);
+console.log(newString2);
+
+const newString3 = "      shubham           ";
+
+console.log(newString3);
+console.log(newString3.trim());
+ 
+
+
+
+
